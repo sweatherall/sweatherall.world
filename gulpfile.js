550de2a786bc1db22a7b1cd99dbfa762eab47a87
S Weatherall <sweather131@gmail.com>
2017-11-11 14:33:07 -0800

    add gulp concat, single style file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+var lessFiles = 'src/**/*.less';
+var lessDest = 'build/css';
 
 gulp.task('html', function(){
   return gulp.src('src/*/*.pug')
@@ -10,10 +16,11 @@ gulp.task('html', function(){
 });
 
 gulp.task('css', function(){
-  return gulp.src('src/*/*.less')
+  return gulp.src(lessFiles)
     .pipe(less())
+    .pipe(concat('styles.css'))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest(lessDest))
 });
 
 gulp.task('default', [ 'html', 'css' ]);
